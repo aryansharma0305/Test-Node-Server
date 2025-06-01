3ed18daf126f5a64d6293ef98e4376957af190f1
Aryan Sharma <aryansharma0305@gmail.com>
2025-06-01 18:21:47 +0530

    Main commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+basicHtml=`<!DOCTYPE html>
+<html>
+<head>
+    <title>My App</title>
+</head>
+<body>
+    <h1>Welcome to My App</h1>
+    <p>This is a simple HTML page served by Express.</p>
+</body>
+</html>
+`
+
+app.get('/', (req, res) => {
+  res.send(basicHtml)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
